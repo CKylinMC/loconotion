776330933f5a560963e7f60b99d58e846b1f5722
CKylinMC <CKylinMC@users.noreply.github.com>
2021-09-24 13:08:09 +0000

    deploy: b7aa61edf06de1c1d1754ab232b6a68e91d13af5

--- 8e3bfad9b85a35f145d934e55a75fa49f0cd57b0.js
+++ 8e3bfad9b85a35f145d934e55a75fa49f0cd57b0.js
@@ -1,3 +1,5 @@
 (function(){
-    console.log("Test OK");
+	fetch("buildtime.txt").then(r=>r.text()).then(r=>console.log("BUILDTIME",r));
+	fetch("buildsha.txt").then(r=>r.text()).then(r=>console.log("BUILDSHA",r));
+	const pgtt=document.querySelector("meta[name='title']").content;if(document.title.indexOf(pgtt)===-1)if(document.title.toLowerCase()==="shadowkylin") document.title = pgtt;else document.title+=" | "+pgtt;
 })()
