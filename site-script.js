b7aa61edf06de1c1d1754ab232b6a68e91d13af5
CKylinMC <11648014+CKylinMC@users.noreply.github.com>
2021-09-24 20:11:52 +0800

    Update site-script.js

--- site-script.js
+++ site-script.js
@@ -1,6 +1,5 @@
 (function(){
-	fetch("buildtime.txt").then(r=>r.text())
-	.then(r=>console.log("BUILDTIME",r));
-	fetch("buildsha.txt").then(r=>r.text())
-	.then(r=>console.log("BUILDSHA",r));
+	fetch("buildtime.txt").then(r=>r.text()).then(r=>console.log("BUILDTIME",r));
+	fetch("buildsha.txt").then(r=>r.text()).then(r=>console.log("BUILDSHA",r));
+	const pgtt=document.querySelector("meta[name='title']").content;if(document.title.indexOf(pgtt)===-1)if(document.title.toLowerCase()==="shadowkylin") document.title = pgtt;else document.title+=" | "+pgtt;
 })()
